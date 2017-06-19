22531e3482552e163813a47cd762004ff68fdf8d
Joel <joel.e.camera@gmail.com>
2017-06-19 12:22:05 -0300

    funcion que <<colapsa>> el dataFrame

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def cml(fns, xs, ys):
     """ Cuadrados minimos lineales.
@@ -26,3 +27,21 @@ def ecm(xs, ys):
     """
     if len(xs) != len(ys): raise Exception('Different size!')
     return sum([(x - y)**2 for x, y in zip(xs, ys)])/len(xs)
+
+def dropKFirst(df, k):
+    """ Quita las primeras k filas de un dataFrame y agrega una nueva columna con el valor anterior de la temperatura.
+        Necesita un dataFrame de dos columnas (pero se podria modificar para que no).
+    """
+    if df.shape[1] != 2: raise Exception('Number of columns different than 2')
+    if k <= 0: raise Exception('k cannot be less or equal than zero')
+    
+    toDrop = list(range(0,k,1))
+    newName = list(df)[1]+'Ant'
+    
+    newCol = list(df[list(df)[1]])
+    del newCol[len(newCol)-1]
+    del newCol[:k-1]
+
+    df.drop(df.index[toDrop],inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df[newName] = pd.Series(newCol)
